316a89745a509581a19f352a3194b98a8a0f6792
macintosh <>
2020-05-25 15:33:45 +0900

    サンプルファイルをアップロード

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
